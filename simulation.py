2239b1e992e847fc57dfbaed91eec317e915dcfb
Matt <0the0judge0@gmail.com>
2015-03-16 21:46:15 +0000

    Allow predefined vector operations

--- simulation.py
+++ simulation.py
@@ -3,26 +3,51 @@ import math
 import time
 
 
-class Vector(object):
+class Vector2(object):
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
-    def magnitude(self):
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        # only works when other is of numeric type
+        return Vector2(self.x * other, self.y * other)
+
+    def __div__(self, other):
+        # only works when other is of numeric type
+        return Vector2(self.x / other, self.y / other)
+
+    def __abs__(self):
         return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def magnitude(self):
+        return self.__abs__()
 
 
 class Particle(object):
     def __init__(self, x, y, q=1, m=100):
         self.charge = q  # charge in Coulombs
         self.mass = m  # mass in kg
-        self.position = Vector(x, y)
-        self.velocity = Vector()
-        self.acceleration = Vector()
-        self._cachedForce = Vector()
-        self.canvas_obj = canvas.create_oval(self.position.x, self.position.y,
-                                             self.position.x+3, self.position.y+3,
+        self.radius = 4
+        self.position = Vector2(x, y)
+        self.velocity = Vector2()
+        self.acceleration = Vector2()
+        self._cachedForce = Vector2()
+        self.canvas_obj = canvas.create_oval(self.position.x-self.radius, self.position.y-self.radius,
+                                             self.position.x+self.radius, self.position.y+self.radius,
                                              fill="red" if q > 0 else "blue")
+
+    def clear_cached_forces(self):
+        self._cachedForce.x = 0
+        self._cachedForce.y = 0
+
+    def add_cached_force(self, f):
+        self._cachedForce += f
 
     def tick(self, dt):
         self.acceleration.x = self._cachedForce.x / self.mass
@@ -52,28 +77,28 @@ class System(object):
 
     def tick(self, dt):
         for p in self.particles:
-            p._cachedForce.x = 0
-            p._cachedForce.y = 0
+            p.clear_cached_forces()
             for q in self.particles:
                 if p != q:
-                    x = q.position.x - p.position.x
-                    y = q.position.y - p.position.y
-                    r = math.sqrt(x**2 + y**2)
-                    F = -self.charge_force(p.charge, q.charge, r) + \
+                    pq = q.position - p.position
+                    r = abs(pq)
+                    force = -self.charge_force(p.charge, q.charge, r) + \
                         self.gravity_force(p.mass, q.mass, r)
-                    Fx = F*x / r
-                    Fy = F*y / r
-                    p._cachedForce.x += Fx
-                    p._cachedForce.y += Fy
+                    p.add_cached_force(pq * force / r)
+
         for p in self.particles:
             p.tick(dt)
+            for q in self.particles:
+                if p != q:
+                    # check for collisions
+                    pass
 
         self.ticks += 1
 
 root = Tk()
 root.title("Simulation")
 
-frame = Frame(root, bd=5)
+frame = Frame(root, bd=0)
 frame.pack()
 canvas = Canvas(frame, width=400, height=300)
 canvas.pack()
@@ -81,18 +106,20 @@ root.update()
 
 
 s = System()
-s.add_particle(Particle(25, 25, -3e-3, 50))
-s.add_particle(Particle(50, 50, 3e-3, 50))
-s.add_particle(Particle(75, 25, 3e-3, 50))
-s.add_particle(Particle(25, 50, -3e-3, 50))
-s.add_particle(Particle(50, 25, 3e-3, 50))
-s.add_particle(Particle(75, 50, -3e-3, 50))
+s.add_particle(Particle(50, 50, -3e-3, 50))
+s.add_particle(Particle(100, 100, 3e-3, 50))
+s.add_particle(Particle(150, 50, 3e-3, 50))
+s.add_particle(Particle(50, 100, -3e-3, 50))
+s.add_particle(Particle(100, 50, 3e-3, 50))
+s.add_particle(Particle(150, 100, -3e-3, 50))
 
 last_tick_time = time.time()
 
+
 while True:
     tick_start_time = time.time()
-    dt = tick_start_time - last_tick_time
+    #dt = tick_start_time - last_tick_time
+    dt = 1.0/60
     s.tick(dt)
     last_tick_time = tick_start_time
     for p in s.particles:
