e83134f63c8b4c32eeb57374c47f5f0988269d5c
Matt <0the0judge0@gmail.com>
2015-03-16 14:13:47 +0000

    PEP8 compliance

--- simulation.py
+++ simulation.py
@@ -2,8 +2,9 @@ from Tkinter import *
 import math
 import time
 
+
 class Vector(object):
-    def __init__(self, x = 0, y = 0):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
@@ -12,16 +13,16 @@ class Vector(object):
 
 
 class Particle(object):
-    def __init__(self, x, y, q = 1, m = 100):
-        self.charge = q # charge in Coulombs
-        self.mass = m # mass in kg
+    def __init__(self, x, y, q=1, m=100):
+        self.charge = q  # charge in Coulombs
+        self.mass = m  # mass in kg
         self.position = Vector(x, y)
         self.velocity = Vector()
         self.acceleration = Vector()
         self._cachedForce = Vector()
-        self.canvas_obj=canvas.create_oval(self.position.x, self.position.y,
-                                           self.position.x+3, self.position.y+3,
-                                           fill = "red" if q > 0 else "blue")
+        self.canvas_obj = canvas.create_oval(self.position.x, self.position.y,
+                                             self.position.x+3, self.position.y+3,
+                                             fill="red" if q > 0 else "blue")
 
     def tick(self, dt):
         self.acceleration.x = self._cachedForce.x / self.mass
@@ -40,17 +41,16 @@ class System(object):
         self.particles = []
         self.ticks = 0
 
-    def addParticle(self, particle):
+    def add_particle(self, particle):
         self.particles.append(particle)
 
-    def chargeForce(self, Q, q, R):
-        return 8.988e9 * Q * q / R ** 2
+    def charge_force(self, q1, q2, r):
+        return 8.988e9 * q1 * q2 / r ** 2
 
-    def gravityForce(self, M, m, R):
-        return 6.67e-34 * M * m / R ** 2
+    def gravity_force(self, m1, m2, r):
+        return 6.67e-34 * m1 * m2 / r ** 2
 
     def tick(self, dt):
-
         for p in self.particles:
             p._cachedForce.x = 0
             p._cachedForce.y = 0
@@ -59,8 +59,8 @@ class System(object):
                     x = q.position.x - p.position.x
                     y = q.position.y - p.position.y
                     r = math.sqrt(x**2 + y**2)
-                    F = -self.chargeForce(p.charge, q.charge, r) + \
-                         self.gravityForce(p.mass, q.mass, r)
+                    F = -self.charge_force(p.charge, q.charge, r) + \
+                        self.gravity_force(p.mass, q.mass, r)
                     Fx = F*x / r
                     Fy = F*y / r
                     p._cachedForce.x += Fx
@@ -69,8 +69,6 @@ class System(object):
             p.tick(dt)
 
         self.ticks += 1
-
-
 
 root = Tk()
 root.title("Simulation")
@@ -83,16 +81,16 @@ root.update()
 
 
 s = System()
-s.addParticle(Particle(25, 25, -5e-3, 30))
-s.addParticle(Particle(50, 50, 3e-3, 70))
-s.addParticle(Particle(75, 25, 3e-3, 50))
-s.addParticle(Particle(25, 50, -8e-3, 130))
-s.addParticle(Particle(50, 25, 4e-3, 70))
-s.addParticle(Particle(75, 50, 1e-3, 10))
+s.add_particle(Particle(25, 25, -3e-3, 50))
+s.add_particle(Particle(50, 50, 3e-3, 50))
+s.add_particle(Particle(75, 25, 3e-3, 50))
+s.add_particle(Particle(25, 50, -3e-3, 50))
+s.add_particle(Particle(50, 25, 3e-3, 50))
+s.add_particle(Particle(75, 50, -3e-3, 50))
 
 last_tick_time = time.time()
 
-while(True):
+while True:
     tick_start_time = time.time()
     dt = tick_start_time - last_tick_time
     s.tick(dt)
@@ -102,8 +100,3 @@ while(True):
     print "dt", dt
     root.update()
     time.sleep(1.0/60)
-
-
-
-
-
