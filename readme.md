c36334acf040dddd1e40bfec12e7c97f84388ddf
mattjudge <0the0judge0@gmail.com>
2016-02-10 21:03:04 +0000

    Added readme.md and example gif
    Also implemented saving frames, for gif creation purposes

---
+++ readme.md
@@ -0,0 +1,15 @@
+#Python Particle Simulator
+
+This was a quick project to introduce a friend to the idea of object oriented programming using classes in python.
+
+It ended up working quite well, taking forces due to charge and mass into account, simulating the movement of particles in a simplistic fasion.
+
+However, it should be noted that collisions are currently not calculated, meaning that the particles can pass inside one another with a distance vector approaching zero, causing large velocities post collision.
+
+###Saving Frames
+Frames can be saved to postscript files, which can then be convreted to gif using a command line untility such as [http://www.imagemagick.org](http://www.imagemagick.org)'s convert, with a command such as `convert -dispose background frame_*.ps anim.gif`.
+
+###Examples
+The example below shows six particles arranged in a grid with equal mass and charge magnitude.
+
+![Example 1](example_01.gif)
--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 from tkinter import *
-import math
 import time
+import subprocess
+from os import makedirs
 
 
 def charge_force(q1, q2, r):
@@ -31,7 +32,7 @@ class Vector2(object):
         return Vector2(self.x / other, self.y / other)
 
     def __abs__(self):
-        return math.sqrt(self.x ** 2 + self.y ** 2)
+        return (self.x ** 2 + self.y ** 2) ** 0.5
 
     def magnitude(self):
         return self.__abs__()
@@ -51,28 +52,28 @@ class Particle(object):
                                              fill="red" if q > 0 else "blue")
 
     def clear_cached_forces(self):
-        self._cachedForce.x = 0
-        self._cachedForce.y = 0
+        self._cachedForce = Vector2()
 
     def add_cached_force(self, f):
         self._cachedForce += f
 
     def tick(self, dt):
-        self.acceleration.x = self._cachedForce.x / self.mass
-        self.acceleration.y = self._cachedForce.y / self.mass
-        self.velocity.x += self.acceleration.x * dt
-        self.velocity.y += self.acceleration.y * dt
+        self.acceleration = self._cachedForce / self.mass
+        self.velocity += self.acceleration * dt
         oldx = self.position.x
         oldy = self.position.y
-        self.position.x += self.velocity.x * dt
-        self.position.y += self.velocity.y * dt
+        self.position += self.velocity * dt
         canvas.move(self.canvas_obj, self.position.x - oldx, self.position.y - oldy)
+
+    def in_frame(self):
+        return 0 <= self.position.x <= canvas.winfo_width() and 0 <= self.position.y <= canvas.winfo_height()
 
 
 class System(object):
     def __init__(self):
         self.particles = []
         self.ticks = 0
+        self.completed = False
 
     def add_particle(self, particle):
         self.particles.append(particle)
@@ -95,36 +96,56 @@ class System(object):
                     # check for collisions
                     pass
 
+        self.completed = len([p for p in self.particles if p.in_frame()]) == 0
         self.ticks += 1
 
-root = Tk()
-root.title("Simulation")
 
-frame = Frame(root, bd=0)
-frame.pack()
-canvas = Canvas(frame, width=400, height=300)
-canvas.pack()
-root.update()
+def save_frame():
+    makedirs('./frames/', exist_ok=True)
+    if s.ticks % 5 == 0 or s.ticks == 1 or s.completed:
+        ps = canvas.postscript(colormode='color', width=canvas.winfo_width(), height=canvas.winfo_height())
+        with open('./frames/frame_{0:06d}.ps'.format(s.ticks), 'w') as f:
+            f.write(ps)
 
 
-s = System()
-s.add_particle(Particle(50, 50, -3e-3, 50))
-s.add_particle(Particle(100, 100, 3e-3, 50))
-s.add_particle(Particle(150, 50, 3e-3, 50))
-s.add_particle(Particle(50, 100, -3e-3, 50))
-s.add_particle(Particle(100, 50, 3e-3, 50))
-s.add_particle(Particle(150, 100, -3e-3, 50))
+def convert_frames():
+    subprocess.check_call('convert -dispose background -delay 8 frame_*.ps anim.gif', shell=True)
 
-last_tick_time = time.time()
 
-while True:
-    tick_start_time = time.time()
-    # dt = tick_start_time - last_tick_time  # realtime
-    dt = 1.0/60  # 60 ticks a second
-    s.tick(dt)
-    last_tick_time = tick_start_time
-    for p in s.particles:
-        print(p.position.x, p.position.y)
-    print("dt", dt)
+if __name__ == "__main__":
+
+    root = Tk()
+    root.title("Simulation")
+
+    frame = Frame(root, bd=0)
+    frame.pack()
+    canvas = Canvas(frame, width=400, height=300)
+    canvas.pack()
     root.update()
-    time.sleep(1.0/60)
+
+    s = System()
+    s.add_particle(Particle(50, 50, -3e-3, 50))
+    s.add_particle(Particle(100, 100, 3e-3, 50))
+    s.add_particle(Particle(150, 50, 3e-3, 50))
+    s.add_particle(Particle(50, 100, -3e-3, 50))
+    s.add_particle(Particle(100, 50, 3e-3, 50))
+    s.add_particle(Particle(150, 100, -3e-3, 50))
+
+    last_tick_time = time.time()
+    frame = 0
+
+    while not s.completed:
+        tick_start_time = time.time()
+        # dt = tick_start_time - last_tick_time  # realtime
+        dt = 1.0/60  # 60 ticks a second
+        s.tick(dt)
+        last_tick_time = tick_start_time
+        for p in s.particles:
+            print(p.position.x, p.position.y)
+        print("dt", dt)
+        root.update()
+        save_frame()
+        time.sleep(1.0/60)
+        frame += 1
+
+    # convert_frames()
